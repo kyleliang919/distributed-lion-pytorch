711edcdc17103cdb88e6ca35c54231a8d7b80228
kaizhaol <kaizhao.liang@sambanovasystems.com>
2023-12-07 21:33:52 +0000

    Adding script for pretraining

---
+++ async_trainer.py
@@ -0,0 +1,25 @@
+import time
+from transformers import Trainer
+from transformers.modeling_utils import unwrap_model
+class AsycTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def _wrap_model(self, model, training=True, dataloader=None):
+        # train/eval could be run multiple-times - if already wrapped, don't re-wrap it again
+        if unwrap_model(model) is not model:
+            return model
+
+        # Multi-gpu training (should be after apex fp16 initialization) / 8bit models does not support DDP
+        if self.args.n_gpu > 1 and not getattr(model, "is_loaded_in_8bit", False):
+            model = model
+
+        if self.args.jit_mode_eval:
+            start_time = time.time()
+            model = self.torch_jit_model_eval(model, dataloader, training)
+            self.jit_compilation_time = round(time.time() - start_time, 4)
+
+        # Note: in torch.distributed mode, there's no point in wrapping the model
+        # inside a DistributedDataParallel as we'll be under `no_grad` anyways.
+        if not training:
+            return model
+        return model
